e48f8c78529036b6b00309dfd00991e60da6cd83
JustinReynaert <Justin.Reynaert@gmail.com>
2017-02-07 15:56:33 +0100

    Wijzigingen

--- projecten_advanced/canvas.js
+++ projecten_advanced/canvas.js
@@ -7,8 +7,10 @@ function rechthoeken(c) {
     var ctx = c.getContext('2d');
 
     if(ctx) {
-        ctx.fillyStyle = "yellow";
+        ctx.fillStyle = "yellow";
         ctx.strokeStyle = "#000";
         ctx.fillRect(50,50,200,70); // x, y, breedte , hoogte
+        ctx.strokeRect (80,80,200,70);
+        ctx.clearRect(100,20,50,160);
     }
 }
